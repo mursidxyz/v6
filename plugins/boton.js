81a3aee131be214cba9da3154fbf4de57751e451
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2023-07-20 23:18:29 +0700

    Update boton.js

--- plugins/boton.js
+++ plugins/boton.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Sukses Menghidupkan ! AkiraaBot 🤖* ')
+    m.reply('*berhasil menghidupkan мυʀѕι∂ вσт-χмℓ*')
 }
 
 
