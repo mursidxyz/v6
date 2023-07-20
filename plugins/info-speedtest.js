4a175f20c5a8b3339f337b05579eb983fa27f852
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2023-07-20 22:44:40 +0700

    Update info-speedtest.js

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -2,7 +2,7 @@ let cp = require ('child_process')
 let { promisify } = require ('util')
 let exec = promisify(cp.exec).bind(cp)
 let handler = async (m, { conn}) => {
-	await conn.reply(m.chat, `Please Wait`, m)
+	await conn.reply(m.chat, `*tunggu sebentar...*`, m)
     let o
     try {
         o = await exec('python3 speed.py --share --secure')
@@ -11,7 +11,7 @@ let handler = async (m, { conn}) => {
     } finally {
         let { stdout, stderr } = o
         if (stdout.trim()) 
-        conn.sendFile(m.chat, `https://telegra.ph/file/ec8cf04e3a2890d3dce9c.jpg`, null, stdout, m)
+        conn.sendFile(m.chat, `https://telegra.ph/file/1e22eb16536dd3f488b48.jpg`, null, stdout, m)
         if (stderr.trim()) m.reply(stderr)
     }
 }
