92ea3301c68b856abc47f5e91604e402e1a64065
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2023-12-17 16:11:34 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,42 +1,56 @@
-const https = require('https');
+const fetch = require("node-fetch");
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) {
-      throw `Masukan URL!\n\ncontoh :\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-    }
-try {
-    if (!args[0].match(/tiktok/gi)) {
-      throw `URL tidak ditemukan!`
-    }
-    m.reply('*Loading...*')
-    const api = await https.get(`https://api.betabotz.org/api/download/tiktok?url=${args[0]}&apikey=${kasan}`, response => {
-      let data = '';
-      response.on('data', chunk => {
-        data += chunk;
-      });
-      response.on('end', async () => {
-        const res = JSON.parse(data);
-        var { 
-          video, 
-          video2, 
-          username,
-          description,
-          audio
-        } = res.result
-        conn.sendFile(m.chat, video, null, `username : ${username}\ndeskripsi : ${description}\naudio : ${audio}`, m);
-      });
-    }).on('error', error => {
-      console.log(error);
-      throw error.message
-    });
-  } catch (e) {
-    console.log(e)
-    throw `terjadi kesalahan saat mengambil link`
-  }
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `Masukkan Link\n\nContoh :\n${usedPrefix + command} https://vt.tiktok.com/ZSLQfMyyj/`;
+  if (!args[0].match(/tiktok/gi)) throw `link tidak ditemukan!`;
+    const arr = [
+    { text: "[▒▒▒▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[█▒▒▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[██▒▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[███▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[████▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[█████▒▒▒▒▒]", timeout: 10 },
+    { text: "[██████▒▒▒▒]", timeout: 10 },
+    { text: "[███████▒▒▒]", timeout: 10 },
+    { text: "[████████▒▒]", timeout: 10 },
+    { text: "[█████████▒]", timeout: 10 },
+    { text: "[██████████]", timeout: 10 },
+    { text: `*berhasil mendapatkan media*\n*video akan segara di kirim*`, timeout: 10 }
+  ];
+
+  const lll = await conn.sendMessage(m.chat, { text: wait }, { quoted: m });
+
+  for (let i = 0; i < arr.length; i++) {
+    await new Promise(resolve => setTimeout(resolve, arr[i].timeout));
+    await conn.relayMessage(m.chat, {
+      protocolMessage: {
+        key: lll,
+        type: 14,
+        editedMessage: {
+          conversation: arr[i].text
+        }
+      }
+    }, {});
+  };  
+		const url = args[0];
+		const apis = await fetch(`https://skizo.tech/api/tiktok?url=${args[0]}&apikey=${xznkey}`);
+		
+var json = await apis.json()
+if(json.msg != "success") throw "error"
+var { 
+play, 
+wmplay, 
+region,
+title,
+duration,
+music
+} = json.data
+await conn.sendFile(m.chat, play, 'tiktok.mp4', `*Deskripsi Video* : ${title}\n*Username Creator* : ${region}\n*Durasi Video* : ${duration} detik\n*Powered* : ${conn.user.name}`, m)
+await conn.sendFile(m.chat, music, 'error.mp3', null, m, true)
+await conn.sendFile(m.chat, wmplay, 'error.mp4', `*ini video yang pakai watermark tiktok*`, m)
 };
-
-handler.help = ['tiktok'];
-handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm)$/i
+handler.help = ['tiktok']
+handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm|dlttwm)$/i
 handler.tags = ['downloader'];
 handler.limit = true;
 handler.group = false;
@@ -46,5 +60,4 @@ handler.admin = false;
 handler.botAdmin = false;
 handler.fail = null;
 handler.private = false;
-
 module.exports = handler;
