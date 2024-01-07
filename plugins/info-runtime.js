83bbffe13a09969fc6bec9756ddaa98ea4a0a66a
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2024-01-07 20:03:04 +0700

    Update info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,43 +1,26 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-
     let _uptime = process.uptime() * 1000
-
     let tio = clockString(_uptime)
-
     let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
 
-var reply = `
-*bot berjalan selama : ${tio}*
+var ct = `
+aktif selama : ${tio}
     `
-
-conn.sendFile(m.chat, `https://telegra.ph/file/9241fe306d5e5b2dd9761.jpg`, null, reply, m)
-
+m.reply(ct)
 }
-
 handler.help = ['runtime']
-
 handler.tags = ['info']
-
 handler.command = /^(uptime|runtime)$/i
 
 module.exports = handler
 
 function clockString(ms) {
-
     let days = Math.floor(ms / (24 * 60 * 60 * 1000));
-
     let daysms = ms % (24 * 60 * 60 * 1000);
-
     let hours = Math.floor((daysms) / (60 * 60 * 1000));
-
     let hoursms = ms % (60 * 60 * 1000);
-
     let minutes = Math.floor((hoursms) / (60 * 1000));
-
     let minutesms = ms % (60 * 1000);
-
     let sec = Math.floor((minutesms) / (1000));
-
-    return days + "hari" + hours + "jam" + minutes + "menit" + sec + "detik";
-
+    return days + " hari " + hours + " jam " + minutes + " menit " + sec + " detik ";
 }
