746c81defce5c6f29ff751d840a1a6be4edb4418
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2024-01-08 07:58:08 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -4,18 +4,18 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `Masukkan link\n\nContoh :\n${usedPrefix + command} https://vt.tiktok.com/ZSNv4wgXN/`;
   if (!args[0].match(/tiktok/gi)) throw `link tidak ditemukan!`;
     const arr = [
-    { text: "[▒▒▒▒▒▒▒▒▒▒]", timeout: 10 },
-    { text: "[█▒▒▒▒▒▒▒▒▒]", timeout: 10 },
-    { text: "[██▒▒▒▒▒▒▒▒]", timeout: 10 },
-    { text: "[███▒▒▒▒▒▒▒]", timeout: 10 },
-    { text: "[████▒▒▒▒▒▒]", timeout: 10 },
-    { text: "[█████▒▒▒▒▒]", timeout: 10 },
-    { text: "[██████▒▒▒▒]", timeout: 10 },
-    { text: "[███████▒▒▒]", timeout: 10 },
-    { text: "[████████▒▒]", timeout: 10 },
-    { text: "[█████████▒]", timeout: 10 },
-    { text: "[██████████]", timeout: 10 },
-    { text: `*berhasil mendapatkan media*\n*video akan segara di kirim*`, timeout: 10 }
+    { text: "[0%▒▒▒▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[█10%▒▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[██20%▒▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[███30%▒▒▒▒▒▒]", timeout: 10 },
+    { text: "[████40%▒▒▒▒▒]", timeout: 10 },
+    { text: "[█████50%▒▒▒▒]", timeout: 10 },
+    { text: "[██████60%▒▒▒]", timeout: 10 },
+    { text: "[███████70%▒▒]", timeout: 10 },
+    { text: "[████████80%▒]", timeout: 10 },
+    { text: "[█████████90%]", timeout: 10 },
+    { text: "[██████████100%]", timeout: 10 },
+    { text: `*berhasil mendapatkan media*\n*video/media akan segara di kirim, mohon tunggu beberapa detik*`, timeout: 10 }
   ];
 
   const lll = await conn.sendMessage(m.chat, { text: wait }, { quoted: m });
@@ -45,9 +45,9 @@ title,
 duration,
 music
 } = json.data
-await conn.sendFile(m.chat, play, 'tiktok.mp4', `*deskripsi Video* : ${title}\n*username Creator* : ${region}\n*durasi video* : ${duration} detik\n*powered* : ${conn.user.name}`, m)
+await conn.sendFile(m.chat, play, 'tiktok.mp4', `*Deskripsi* : ${title}\n*Username* : ${region}\n*Durasi* : ${duration} detik\n*powered* : ${conn.user.name}`, m)
 await conn.sendFile(m.chat, music, 'error.mp3', null, m, true)
-await conn.sendFile(m.chat, wmplay, 'error.mp4', `*ini video yang pakai tiktok*`, m)
+await conn.sendFile(m.chat, wmplay, 'error.mp4', `ig : @mursid.st\nini video yang pakai watermark`, m)
 };
 handler.help = ['tiktok']
 handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm|dlttwm)$/i
